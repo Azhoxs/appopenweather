a0379fc13d5d36cc26532ec3afad97e72c05fab3
Erwan Verscheure <erwanverscheure@gmail.com>
2024-03-21 17:17:21 +0100

    Création du app.dart et liaison avec le main.dart

--- lib/menu/menu_route.dart
+++ lib/menu/menu_route.dart
@@ -2,24 +2,34 @@ import 'package:flutter/material.dart';
 
 import '../routes/home_route.dart';
 
-
 class MenuRoute extends StatelessWidget {
   const MenuRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-          children: [
-            Center(
-              child: ElevatedButton(
-
-                onPressed: () => _goToHome(context),
-                child: const Text("GO"),
-              ),
-            ),
-          ],
-        )
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.cloud),
+            label: 'Météo',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_city),
+            label: 'Villes',
+          ),
+        ],
+        onTap: (index) {
+          switch (index) {
+            case 0:
+            // Action when 'Météo' button is pressed
+              break;
+            case 1:
+              _goToHome(context); // Action when 'Villes' button is pressed
+              break;
+          }
+        },
+      ),
     );
   }
 
--- lib/routes/home_route.dart
+++ lib/routes/home_route.dart
@@ -6,8 +6,28 @@ class HomeRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("IMC Calculator Ultra Pro Max+"),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1, // Set 'Villes' button as selected
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.cloud),
+            label: 'Météo',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_city),
+            label: 'Villes',
+          ),
+        ],
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              Navigator.pop(context); // Action when 'Météo' button is pressed
+              break;
+            case 1:
+            // Action when 'Villes' button is pressed
+              break;
+          }
+        },
       ),
     );
   }
