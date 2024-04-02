9e1cdbdd1420af0f39cb5a47e702a63bf7f52754
evrsch25 <erwan.verscheure@etu.univ-littoral.fr>
2024-04-02 14:39:32 +0200

    Ajout d'un fond en fonction de l'heure

--- lib/routes/menu/menu_route.dart
+++ lib/routes/menu/menu_route.dart
@@ -37,40 +37,68 @@ class _MenuRouteState extends State<MenuRoute> {
   }
 
   Widget _buildBody() {
+    // Obtenez l'heure actuelle
+    int hour = DateTime.now().hour;
+
+    // Déterminez le nom de l'image de fond en fonction de l'heure
+    String backgroundImage;
+    if (hour >= 6 && hour < 11) {
+      backgroundImage = 'backgrounds/1.jpg';
+    } else if (hour >= 11 && hour < 18) {
+      backgroundImage = 'backgrounds/2.jpg';
+    } else if (hour >= 18 && hour < 21) {
+      backgroundImage = 'backgrounds/3.jpg';
+    } else {
+      backgroundImage = 'backgrounds/4.jpg';
+    }
+
     switch (_currentIndex) {
       case 0:
-        return FutureBuilder<String>(
-          future: WeatherTemperature.getTemperature('Paris'),
-          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+        return FutureBuilder<double>(
+          future: WeatherTemperature.getTemperature('Paris').then((value) => double.parse(value)),
+          builder: (BuildContext context, AsyncSnapshot<double> snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return const Center(child: CircularProgressIndicator());
             } else {
               if (snapshot.hasError)
                 return Center(child: Text('Erreur: ${snapshot.error}'));
-              else
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        snapshot.data != null
-                            ? '${double.parse(snapshot.data!).round()}°C'
-                            : 'Chargement...',
-                        style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        'Paris',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    ],
+              else {
+                int roundedTemperature = snapshot.data?.round() ?? 0;
+                return Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(backgroundImage),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '$roundedTemperature°C',
+                          style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold, color: Colors.white),
+                        ),
+                        Text(
+                          'Paris',
+                          style: TextStyle(fontSize: 40, color: Colors.white),
+                        ),
+                      ],
+                    ),
                   ),
                 );
+              }
             }
           },
         );
       case 1:
         return Container(
-          // Contenu de la page Villes
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(backgroundImage),
+              fit: BoxFit.cover,
+            ),
+          ),
           child: Center(
             child: Text('Contenu de la page Villes'),
           ),
